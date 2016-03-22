dc20e0255701202af79608fd2c599bfd14032c20
Anthony Gold and LaTaevia Berry <anthony@>
2016-03-22 09:17:57 -0700

    add jquery .click practice

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $ ("h1").click(function() {
+    alert("This is a header.");
+    alert("More than one thing");
+  });
+  $ ("p").hover(function(){
+    alert("This is a paragraph");
+  });
+  $ ("img").click(function(){
+    alert("This is an image");
+  });
+  $ ("h2").click(function(){
+    alert("Holy Crap it's Header 2");
+  });
+  $("ul").dblclick(function() {
+    alert("OMG! A list element! An unordered list element!");
+  });
+});

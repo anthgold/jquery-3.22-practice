911ce03cbe222968d572324edf3f4dc6ef561ad7
Anthony Gold and LaTaevia Berry <anthony@>
2016-03-22 10:09:11 -0700

    add toggle practice with edits to css, html, and JS, also added face eating walrus

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
-  $ ("h1").click(function() {
+  $("h1").click(function() {
     alert("This is a header.");
     alert("More than one thing");
   });
-  $ ("p").hover(function(){
+  $("p").dblclick(function(){
     alert("This is a paragraph");
   });
-  $ ("img").click(function(){
+  $("img").click(function(){
     alert("This is an image");
   });
-  $ ("h2").click(function(){
+  $("h2").click(function(){
     alert("Holy Crap it's Header 2");
   });
   $("ul").dblclick(function() {
     alert("OMG! A list element! An unordered list element!");
+  });
+  $(".clickable").click(function() {
+    $("#initially-hidden").toggle();
+    $("#initially-showing").toggle();
   });
 });
